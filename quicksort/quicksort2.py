2254e5a4164b83f4506a891b703291715754cd23
WeaponMaster <1925730614@qq.com>
2020-05-19 21:28:56 +0800

    修改完善了部分代码

--- quicksort/quicksort2.py
+++ quicksort/quicksort2.py
@@ -1,17 +1,28 @@
+"""单指针遍历法"""
 
-def quick_sort(array, l, r):
+from randomList import randomList
+
+
+def quickSort(array, l, r):
     if l < r:
         q = partition(array, l, r)
-        quick_sort(array, l, q - 1)
-        quick_sort(array, q + 1, r)
+        quickSort(array, l, q - 1)
+        quickSort(array, q + 1, r)
 
 
 def partition(array, l, r):
-    x = array[r]
+    pivot = array[r]
     i = l - 1
     for j in range(l, r):
-        if array[j] <= x:
+        if array[j] <= pivot:
             i += 1
             array[i], array[j] = array[j], array[i]
     array[i + 1], array[r] = array[r], array[i + 1]
     return i + 1
+
+
+if __name__ == '__main__':
+    iList = randomList.randomList(20)
+    print("原列表为：%s" % iList)
+    quickSort(iList, 0, len(iList) - 1)
+    print("新列表为：%s" % iList)
